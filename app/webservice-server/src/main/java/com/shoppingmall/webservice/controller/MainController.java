2bea95b7a06c31a89e2418d81978b1d3a1c09438
wallybum <elvvis93@gmail.com>
2023-03-07 20:11:30 +0900

    Initial Commit

---
+++ app/webservice-server/src/main/java/com/shoppingmall/webservice/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.shoppingmall.webservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping({"/"})
+    public String initMain() {
+        return "main/main";
+    }
+}
